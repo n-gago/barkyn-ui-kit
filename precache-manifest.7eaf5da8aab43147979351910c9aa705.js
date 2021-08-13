d42c874a068e96fdd07b57b369196961262148c9
Nuno Gago <nunogago@Nunos-MBP.lan>
2021-08-13 19:34:57 +0100

    Updates

---
+++ precache-manifest.7eaf5da8aab43147979351910c9aa705.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7728dd6ab6e9ca13267521f39dd92f9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c830a267b5224a12e713",
+    "url": "./static/css/main.443fda26.chunk.css"
+  },
+  {
+    "revision": "f11a2fa3465c380a2933",
+    "url": "./static/js/2.fc608f50.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.fc608f50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c830a267b5224a12e713",
+    "url": "./static/js/main.37e28c8f.chunk.js"
+  },
+  {
+    "revision": "f2c287343a971313a54b",
+    "url": "./static/js/runtime-main.2447f59a.js"
+  }
+]);
